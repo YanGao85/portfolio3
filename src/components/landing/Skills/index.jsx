fbeac861793b7bc0bcd8b1df944177179b9714eb
Yan <ing.yangao@gmail.com>
2020-04-10 18:00:00 -0400

    new build

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -11,11 +11,8 @@ export const Skills = () => (
         <img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
       </Thumbnail>
       <Details>
-        <h1>Hi There!</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s
-          standard dummy.
-        </p>
+        <h1>Technical Skills</h1>
+        <p>JavaScript, React, Redux, Node.js, Express.js, Sequelize.js, Firebase, SpeechToText API, GraphQL, Git</p>
         <Button as={AnchorLink} href="#contact">
           Hire me
         </Button>
